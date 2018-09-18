04b9ee7e164c54f9431b0e0ab71f862d3269dde3
Paolo Monni <paolo.monni345@gmail.com>
2018-09-18 21:57:45 +0200

    In navbar user profile redirect to user_profile.html

--- django_forum/core/views.py
+++ django_forum/core/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def homepage(request):
     return render(request, 'core/homepage.html')
+
+def userProfileView(request, username):
+    user = get_object_or_404(User, username=username)
+    context ={"user": user}
+    return render(request, 'core/user_profile.html', context)
+    
