b96d8e74413156ff78fb2d671b123fef5934cbaa
Paolo Monni <paolo.monni345@gmail.com>
2018-09-20 23:02:22 +0200

    Homepage with links to section

--- django_forum/forum/models.py
+++ django_forum/forum/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -13,6 +14,9 @@ class Section(models.Model):
 
     def __str__(self):
         return self.name_section
+    def get_absolute_url(self):
+        return reverse("view_section", kwargs={"pk": self.pk})
+    
 
 class Discussion(models.Model):
     title = models.CharField(max_length=120)
