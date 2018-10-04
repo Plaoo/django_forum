c3e50264c4a0ca81126793130d26773e3c09755b
Paolo Monni <paolo.monni345@gmail.com>
2018-10-04 17:14:14 +0200

    Search function

--- django_forum/core/urls.py
+++ django_forum/core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name="homepage"),
     path('users/', views.UserList.as_view(), name="user_list"),
     path('user/<username>/', views.userProfileView, name="user_profile"),
+    path('search/', views.search, name="fun_search")
         
 ]
--- django_forum/core/views.py
+++ django_forum/core/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
-from forum.models import Discussion, Section
+from forum.models import Discussion, Section, Post
 
 
 # Create your views here.
@@ -24,4 +24,19 @@ def userProfileView(request, username):
     discussion_user = Discussion.objects.filter(author = user).order_by("-pk")
     context ={"user": user, "discussion_user":discussion_user}
     return render(request, 'core/user_profile.html', context)
+
+def search(request):
+    if "q" in request.GET:
+        querystring = request.GET.get("q")
+        if len(querystring) == 0:
+            return redirect("/search/")
+        discussions = Discussion.objects.filter(title__icontains=querystring)
+        posts = Post.objects.filter(content__icontains=querystring)
+        users = User.objects.filter(username__icontains=querystring)
+        context = {"discussions":discussions,
+                   "posts":posts,
+                   "users":users}
+        return render(request, 'core/search.html', context)
+    else:
+        return render(request, 'core/search.html')
 
